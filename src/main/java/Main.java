0ce483e6a6e08577cd07909692524a6eccc08837
amirah2700 <amir2700@hotmail.com>
2022-09-23 09:50:56 +0200

    Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import javax.naming.MalformedLinkException;
+import javax.xml.crypto.Data;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+UserInterface ui = new UserInterface();
+ui.startProgram();
+    }
+}
+
